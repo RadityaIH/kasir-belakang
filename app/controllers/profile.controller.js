27d7a7d77de301d3011155ff596dd9153b6f7401
RadityaIH <radityadave10@gmail.com>
2024-01-31 10:36:22 +0700

    profile

--- app/controllers/profile.controller.js
+++ app/controllers/profile.controller.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken'
 import db from '../../db/db.js'
 import dotenv from "dotenv";
+import crypto from 'crypto'
 
 dotenv.config();
 
@@ -17,7 +18,7 @@ export const getUser = (req, res) => {
         const decoded = jwt.verify(token, SECRET);
         const userId = decoded.userId
 
-        const sql = `SELECT u.username, k.nama, r.namarole FROM users u JOIN kasir k JOIN role r WHERE u.id = k.id AND u.idrole = r.idrole AND u.id = ${userId};`
+        const sql = `SELECT u.username, u.nama, r.namarole FROM users u JOIN role r WHERE u.idrole = r.idrole AND u.id = ${userId};`
 
         db.query(sql, (err, result) => {
             if (err) {
@@ -31,6 +32,58 @@ export const getUser = (req, res) => {
 
             res.json({ success: true, username: user.username, nama: user.nama, role: user.namarole})
         })
+    } catch (error) {
+        return res.status(401).json({ error: "Unauthorized"})
+    }
+}
+
+export const updateUser = (req, res) => {
+    const token = req.cookies.token
+    const { username, nama_lengkap, password_lama, password_baru } = req.body;
+
+    if (!token) {
+        return res.status(401).json({ error: "Unauthorized"})
+    }
+
+    try {
+        const decoded = jwt.verify(token, SECRET);
+        const userId = decoded.userId
+
+        if (password_lama || password_baru) {
+            const hashedPassword = crypto.createHash('md5').update(password_lama).digest('hex');
+    
+            const sql = `SELECT password FROM users WHERE BINARY password = ? AND id = ${userId}`
+            db.query(sql, [hashedPassword], (err, result) => {
+                if (err) {
+                    return res.status(500).json({ error: "Internal Server Error"});
+                }
+    
+                if (result.length === 0) {
+                    return res.status(401).json({ error: "Password lama salah"});
+                }
+    
+                const hashedNewPassword = crypto.createHash('md5').update(password_baru).digest('hex');
+    
+                const updtSql = `UPDATE users SET username = ?, nama = ?, password = ? WHERE id = ${userId}`
+                db.query(updtSql, [username, nama_lengkap, hashedNewPassword], (err, result) => {
+                    if (err) {
+                        return res.status(500).json({ error: "Internal Server Error"});
+                    }
+    
+                    res.json({ success: true, message: "Update Success"})
+                })
+            })
+        } else {
+            const updtSql = `UPDATE users SET username = ?, nama = ? WHERE id = ${userId}`
+            db.query(updtSql, [username, nama_lengkap], (err, result) => {
+                if (err) {
+                    return res.status(500).json({ error: "Internal Server Error"});
+                }
+
+                res.json({ success: true, message: "Update Success"})
+            })
+        }
+
     } catch (error) {
         return res.status(401).json({ error: "Unauthorized"})
     }
--- app/routes/profile.routes.js
+++ app/routes/profile.routes.js
@@ -2,6 +2,7 @@ import * as controller from '../controllers/profile.controller.js';
 
 const profileRoutes = (app) => {
     app.get('/getUser', controller.getUser)
+    app.put('/updateUser', controller.updateUser)
 }
 
 export default profileRoutes
