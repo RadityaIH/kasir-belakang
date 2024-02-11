95c7d2fb0570d768817dce793a4f01b608640527
RadityaIH <radityadave10@gmail.com>
2024-02-12 01:01:04 +0700

    db

--- db/db.js
+++ db/db.js
@@ -1,10 +1,25 @@
 import mysql from 'mysql'
+import dotenv from "dotenv";
+dotenv.config();
 
-const db = mysql.createConnection({
-    host: "localhost", 
-    user: "root", 
-    password: "", 
-    database: "kasir"
-})
+const options = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: process.env.DB_ENABLE_SSL === 'true' ? {
+        minVersion: 'TLSv1.2',
+        ca: process.env.TIDB_CA_PATH ? fs.readFileSync(process.env.TIDB_CA_PATH) : undefined
+     } : null,
+ }
+const db = mysql.createConnection(options)
+console.log(process.env.DB_HOST)
+// const db = mysql.createConnection({
+//     host: "gateway01.ap-southeast-1.prod.aws.tidbcloud.com", 
+//     user: "NwcVrKHz8mQUFbQ.root", 
+//     password: "", 
+//     database: "kasir"
+// })
 
 export default db
