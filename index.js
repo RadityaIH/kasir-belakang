938675659b8046d999cb9578078ddc96863b2ee0
RadityaIH <radityadave10@gmail.com>
2024-03-23 13:21:43 +0700

    bisa plis

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
-// export { upload };
+export { upload };
 
 // Add this line to serve static files
 app.use('/uploads', express.static('uploads'));
